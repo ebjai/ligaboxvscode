a329c85b40953be9a55fe594e0ebd5679f0fb440
ebjai <eddiebarberiz@gmail.com>
2025-10-20 19:21:19 -0400

    Remove Vite configuration file
    delete accidental path

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,1 @@
-import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    open: true
-  },
-  preview: {
-    port: 4173
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
-});
